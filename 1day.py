0501ecc9dc4653778cd2787bd4471b396161a975
parmveer99solanki99-pixel <parmveer99solanki99@gmail.com>
2026-05-26 13:46:40 +0530

    Add files via upload

---
+++ 1day.py
@@ -0,0 +1,17 @@
+# Variable:- container to store values through memory space occupation.
+
+# a = 10
+# print(a)
+# print(id(a))
+# print(type(a))
+
+# same values of two different variables will have same id because of memory optimization by python.
+
+# a = 10
+# b = 10
+# print(id(a))
+# print(id(b))
+
+a = int(input("Enter  the number: "))
+print(a)
+print(type(a))
