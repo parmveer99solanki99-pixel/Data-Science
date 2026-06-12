46bc783d81b2f1599424f200e032f0c13db09d1e
parmveer99solanki99-pixel <parmveer99solanki99@gmail.com>
2026-06-12 17:48:13 +0530

    Add commented-out pandas code examples

---
+++ 13day.py
@@ -0,0 +1,39 @@
+# import pandas as pd
+
+# df = pd.Series([10, 20, 30])
+# print(df)
+# print(df[0]) 
+
+
+# dict
+
+# dict = {"Name": ["Paramveer", "Rohit", "Nikhil"],
+#         "Age": [20, 21, 19],
+#         "Salary": [{"In-hand": "15000", "ctc": "1.2lpa"},
+#                    {"In-hand": "25000", "ctc": "3.2lpa"},
+#                    {"In-hand": "40000", "ctc": "4.2lpa"}
+#                    ]
+#         } 
+
+# df = pd.DataFrame(data = dict)
+# print(df)
+
+
+# import pandas as pd
+# data = {
+#     "Name": ["dheeraj", "kunal", "abhishek", "praveen", "anjali"],
+#     "Marks": [123, 1234, 12345, 123456, 1234567]
+# }
+
+# df = pd.DataFrame(data)
+# df.to_csv("file1.csv", index = False)
+
+# print("csv file saved successfully")
+
+# import pandas as pd
+# url = "https://raw.githubusercontent.com/rajendra0968jangid/Ds-Arya/main/file2%20-%20Sheet1.csv"
+# df = pd.read_csv(url)
+# print(df)
+
+
+
