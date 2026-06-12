cf8e8611a78bd2d2058e01a14bce21db422d138d
parmveer99solanki99-pixel <parmveer99solanki99@gmail.com>
2026-06-12 17:35:33 +0530

    Add initial Python code examples for data types and operators

---
+++ 2day.py
@@ -0,0 +1,150 @@
+# # integer
+# a = 10 
+# print(a)
+# print(type(a))
+
+# # float
+# b = 10.10
+# print(b)
+# print(type(b))
+
+# # String
+# c = "Hello World"
+# print(c)
+# print(type(c))
+
+# # boolean
+# d = True
+# print(d)
+# print(type(d))
+
+
+# Type converstion & Input/output
+ 
+# num = input("Enter a number: ")
+# print(num)
+# print(type(num))
+# print(int(num) + 10)
+# print(num + str(10))
+
+
+
+# Operartors
+
+# Arithmetic operators (+,-,*,/,//,%,**) :- result is always a value.
+
+# a = 30
+# b = 20
+# print(a + b) # addition
+
+# c = 20
+# d = 10
+# print(c - d) # subtraction
+
+# e  = 10
+# f = 5
+# print(e * f) # multiplication
+
+# num1 = 20
+# num2 = 2
+# print(num1 / num2) # float division
+
+# num3 = 20
+# num4 = 3
+# print(num3 // num4) # floor division
+
+# num5 = 10
+# num6 = 3
+# print(num5 % num6) # modulus operator
+
+# num7 = 5 
+# num8 = 3
+# print(num7 ** num8) # exponentiation operator
+
+
+
+# num1 = int(input("Enter number 1: "))
+# num2 = 2
+
+# if(num1 % num2 == 0):
+#         print(f"{num1} is even ")
+        
+# else:
+#         print(f"{num1} is odd ")
+
+
+
+# Reletional operators (>,<,>=,<=,==,!=) :- result is always a boolean value.
+
+# num1 = 10
+# num2 = 11
+# print(num1 == num2) # equal to
+
+# num1 = 10
+# num2 = 11
+# print(num1 < num2) # less than
+
+# num1 = 10
+# num2 = 11
+# print(num1 > num2) # greater than
+
+# num1 = 10
+# num2 = 11
+# print(num1 <= num2) # less than or equal to
+
+# num1 = 10
+# num2 = 11
+# print(num1 >= num2) # greater than or equal to
+
+# num1 = 10
+# num2 = 11
+# print(num1 != num2) # not equal to
+
+
+
+# Assignment operators (=,+=,-=,*=,/=,//=,%=,**=) :- result is always a value.
+
+# a = 10
+# b = a + 20
+# print(b)  # =
+
+# a = 10
+# a += 20
+# print(a)  # +=
+
+# a = 10
+# a -= 5
+# print(a)  # -=
+
+# a = 10
+# a *= 2
+# print(a)  # *=
+
+# a = 10
+# a /= 2
+# print(a)  # /=
+
+# a = 10
+# a //= 3
+# print(a)  # //=
+
+# a = 10
+# a %= 3
+# print(a)  # %=
+
+# a = 10
+# a **= 2
+# print(a)  # **=
+
+
+
+# Logical Operator: - and, or, not.
+
+a = 10
+b = 20
+print(a and b) # and
+
+a = -10
+b = 10
+print(a or b) 
+
