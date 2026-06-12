f39cfbb2e37b5a1670692dba2607db48d46ba2dc
parmveer99solanki99-pixel <parmveer99solanki99@gmail.com>
2026-06-12 17:40:15 +0530

    Add examples of math module usage

---
+++ 6day.py
@@ -0,0 +1,35 @@
+# Modules:
+
+# example 1
+# import math
+
+# print(math.pi)
+# print(math.sqrt(25))
+# print(math.factorial(5))
+# print(math.pow(2, 3))
+# print(math.ceil(2.3))
+# print(math.floor(2.3))
+# print(math.e)
+
+
+
+# example 2
+
+# from math import factorial, pi
+# print(factorial(5))
+# print(pi)
+
+
+# example 3
+
+from math import *
+print(pow(5, 3))
+print(pi)
+print(floor(10.32))
+
+
+
+# File Handling
+
+
+
