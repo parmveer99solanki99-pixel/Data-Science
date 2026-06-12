381e48f7a07449da15b7f5eabde1b8390051dae1
parmveer99solanki99-pixel <parmveer99solanki99@gmail.com>
2026-06-12 17:45:25 +0530

    Add NUMPY examples and reshape functionality

---
+++ 9day.py
@@ -0,0 +1,34 @@
+import numpy
+#import numpy as np
+#from numpy import arange
+#from numpy import *
+
+# n = numpy.arange(11)
+# print(type(n))
+# print(n)
+
+
+# example
+
+# n = numpy.arange(12)
+# d = n.reshape(3, 4)
+# print(type(d))
+# print(d)
+
+# example 2d
+
+# n = numpy.arange(12).reshape(3, 4)
+# print(n)
+
+
+# example 3d
+
+# n = numpy.arange(24).reshape(2, 3, 4)
+# print(n)
+
+
+# example split & 2, 5, 8, 11 -> print
+
+n = numpy.arange(12).reshape(4, 3)
+print(n)
+print(n[0:1])
